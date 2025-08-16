7f6664d8eb1bb154f7970b80d2a07484b882b101
Igor Barros <igorblopes1993@gmail.com>
2025-08-16 15:49:42 -0300

    Criando services

--- SDK/src/modules/Backend/index.ts
+++ SDK/src/modules/Backend/index.ts
@@ -1,7 +1,21 @@
+import { CortexDatabase } from './infra/database/cortex-db';
+import { RootDatabase } from './infra/database/root-db';
+import { FraudAssessment } from './interfaces';
+import { SenseScoreModelDB } from './interfaces-db';
+import { FraudServices } from './services/fraud-services';
+import { SenseServices } from './services/sense-services';
+
+
 export class BackendSDK {
+    rootDB = new RootDatabase();
+    db = new CortexDatabase(this.rootDB);
 
     init() {
-        console.log("Backend SDK iniciado");
+        console.log("Backend SDK iniciando...");
+        
+        this.db.init();
+
+        console.log("Backend SDK iniciado!");
     }
 
     test(): any {
@@ -9,5 +23,22 @@ export class BackendSDK {
             "Teste": "teste"
         };
     }
+
+    async getAllSenseScores(): Promise<SenseScoreModelDB[]>{
+        let senseService = new SenseServices(this.db.senseScoreDB);
+        return await senseService.getAllSenseScore();
+    }
+
+     async getLevelByScore(score: string): Promise<string> {
+        let senseService = new SenseServices(this.db.senseScoreDB);
+        return await senseService.getLevelByScore(score);
+     }
+
+    async getFraudByAccountHash(accountHash: string): Promise<FraudAssessment[]> {
+        let fraudService = new FraudServices(this.db.fraudDB);
+        return await fraudService.findFraudByAccountHash(accountHash);
+    }
+
+    
     
 }
